04c5e9fb28cb40178433e19f6d1e4e8c84fb325e
gambtho <thomasgamble2@gmail.com>
2019-08-06 06:19:21 -0700

    alias cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias reload!='. ~/.zshrc'
 alias vi='nvim'
 alias code="/mnt/c/Users/thgamble/AppData/Local/Programs/Microsoft\ VS\ Code\ Insiders/Code\ -\ Insiders.exe"
 alias ls='ls --color=auto'
+alias sd='eval $(ssh-agent -s) && ssh-add'
