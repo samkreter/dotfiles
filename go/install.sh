9207c84d29848d1007b1464bda5110df63a9090b
gambtho <thgamble@microsoft.com>
2019-09-10 11:12:41 -0400

    cleanup of docker and go installs

--- go/install.sh
+++ go/install.sh
@@ -1,12 +1,13 @@
-GO_VERSION=1.12.7
+GO_VERSION=1.13.1
 
 if test ! $(which go)
 then
   echo "  Installing gofor you."
   curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
-  sudo tar xvf go${GO_VERSION}.linux-amd64.tar.gz -C /usr/local/go
+  sudo mkdir -p /usr/local/go
+  sudo tar xvf go${GO_VERSION}.linux-amd64.tar.gz -C /usr/local
   rm go${GO_VERSION}.linux-amd64.tar.gz
   source ./path.zsh
-  go get golang.org/x/tools/cmd/godoc
-  go get github.com/golang/lint/golint
+  # go get golang.org/x/tools/cmd/godoc
+  # go get github.com/golang/lint/golint
 fi
