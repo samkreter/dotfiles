89f7b7380371a82c3dd2006b358fb8a24fdea5b8
gambtho <thgamble@microsoft.com>
2019-09-10 10:37:56 -0400

    docker autoapprove

--- docker/install.sh
+++ docker/install.sh
@@ -3,6 +3,6 @@ then
   echo "> Installing docker for you."
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-  sudo apt update
-  sudo apt install docker-ce
+  sudo apt update -y
+  sudo apt install docker-ce -y 
 fi
