aee8ede2200a61e93d078a3d612df95bf92879de
gambtho <thgamble@microsoft.com>
2019-12-14 19:44:00 -0500

    alias addition and pathing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias vi='nvim'
 #alias code="/mnt/c/Users/thgamble.NORTHAMERICA/AppData/Local/Programs/Microsoft\ VS\ Code\ Insiders/Code\ -\ Insiders.exe"
 alias ls='ls --color=auto'
 alias sd='eval $(ssh-agent -s) && ssh-add'
+alias bfg='java -jar /usr/local/bin/bfg.jar'
 
 function pbcopy() {
    (cat "$@" | clip.exe)
