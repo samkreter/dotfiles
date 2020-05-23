2e75723edc4a11b8ff4aa4139b10b849a8b18140
gambtho <Thomas.Gamble@microsoft.com>
2020-05-23 14:17:14 -0400

    comment out rust install

--- rust/install.sh
+++ rust/install.sh
@@ -1,5 +1,5 @@
-if [ ! -x "$(which rustc)" ]
-then
-  echo "rust install instructions...."
-  echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
-fi
+# if [ ! -x "$(which rustc)" ]
+# then
+#   echo "rust install instructions...."
+#   echo "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+# fi
