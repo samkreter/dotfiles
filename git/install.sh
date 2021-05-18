d23af7c64a023a09e8387d3a5ac0fbddb8e093bd
Sam Kreter <Thomas.Gamble@microsoft.com>
2021-05-18 12:08:42 -0400

    updating git config correctly

--- git/install.sh
+++
@@ -1,6 +0,0 @@
-git config --global alias.go checkout
-git config --global alias.b branch
-git config --global alias.cm 'commit -m'
-git config --global alias.s status
-git config --global alias.a 'add .'
-git config --global alias.ph 'push origin head'
