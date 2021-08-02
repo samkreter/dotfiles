bc0922d1875c8ef7373f177b4b29b509d32fdccf
Sam Kreter <samkreter@gmail.com>
2021-08-02 09:09:35 -0400

    adding updated dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,16 @@
 alias reload!='. ~/.zshrc'
-alias updatem='git go master; git pull origin master'
+
+alias updatem='git go master; git pull origin master &> /dev/null || git go main; git pull origin main'
 
 # Python Venv
-alias act='. .venv/bin/activate'
+alias acti='. .venv/bin/activate &> /dev/null || . venv/bin/activate'
 alias dact='deactivate'
 
 #GiveDirectly
-alias gd='cd ~/givedirectly'
+alias gd='cd ~/givedirectly/givedirectly'
+
+# Utitlies
+alias encrypt='openssl des -in'
+alias decrypt='openssl des -d -in'
+
+alias vsuniq='echo ^(.*)(\n\1)+$'
