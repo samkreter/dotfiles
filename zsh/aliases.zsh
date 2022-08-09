068786e04861e0ffb4510c57e7e511a662f7532b
Sam Kreter <samkreter@gmail.com>
2022-08-09 10:35:34 -0400

    adding aws resources

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias reload!='. ~/.zshrc'
 
-alias updatem='git go master; git pull origin master &> /dev/null || git go main; git pull origin main'
+alias updatem='git go main; git pull origin main'
 
 # Python Venv
 alias acti='. .venv/bin/activate &> /dev/null || . venv/bin/activate'
@@ -9,8 +9,15 @@ alias dact='deactivate'
 #GiveDirectly
 alias gd='cd ~/givedirectly/givedirectly'
 
+# AWS 
+alias aprod='export AWS_PROFILE=prod'
+alias adev='export AWS_PROFILE=dev'
+alias awhich='echo $AWS_PROFILE'
+
+
 # Utitlies
 alias encrypt='openssl des -in'
 alias decrypt='openssl des -d -in'
 
 alias vsuniq='echo ^(.*)(\n\1)+$'
+
